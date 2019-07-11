13b4650e11b7c0b2a8b4a34ffe57c445246ef412
Mati Andreas <mati.andreas@ambientia.ee>
2019-07-11 11:52:50 +0300

    AMBESTINT-80 add entity

---
+++ src/QueueCommandEntity.php
@@ -0,0 +1,188 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Ambientia QueueCommand package.
+ *
+ * (c) Ambientia Estonia OÜ
+ */
+
+namespace Ambientia\QueueCommand;
+
+use DateTime;
+
+
+/**
+ * @author mati.andreas@ambientia.ee
+ */
+class QueueCommandEntity
+{
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * Service container id
+     * Service must be public for usage
+     *
+     * @var string
+     */
+    private $service;
+
+    /**
+     * array of command arguments
+     *
+     * @var array
+     */
+    private $arguments = [];
+
+    /**
+     * Offset executing time
+     * if not set, command will be executed as soon as possible
+     *
+     * @var DateTime|null
+     */
+    private $ttl;
+
+    /**
+     * command status
+     *
+     * @var string|null
+     */
+    private $status;
+
+    /**
+     * @var DateTime|null
+     * @internal
+     */
+    private $started;
+
+    /**
+     * @var DateTime|null
+     * @internal
+     */
+    private $ended;
+
+    /**
+     * Messages related to executing command
+     * Set also if anything is returned by command
+     *
+     * @var string|null
+     */
+    private $message;
+
+    /**
+     * Execute order
+     *
+     * @var int
+     */
+    private $priority = 0;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setService(string $service): void
+    {
+        $this->service = $service;
+    }
+
+    public function getService(): string
+    {
+        return $this->service;
+    }
+
+    /*
+     * @param array $arguments
+     */
+    public function setArguments(array $arguments): void
+    {
+        $this->arguments = $arguments;
+    }
+
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    public function setTtl(DateTime $ttl): void
+    {
+        $this->ttl = $ttl;
+    }
+
+    public function getTtl(): ?DateTime
+    {
+        return $this->ttl;
+    }
+
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param DateTime $started
+     *
+     * @internal
+     */
+    public function setStarted(DateTime $started): void
+    {
+        $this->started = $started;
+    }
+
+    /**
+     * @return DateTime|null
+     * @internal
+     */
+    public function getStarted()
+    {
+        return $this->started;
+    }
+
+    /**
+     * @param DateTime $ended
+     *
+     * @internal
+     */
+    public function setEnded(DateTime $ended): void
+    {
+        $this->ended = $ended;
+    }
+
+    /**
+     * @return DateTime|null
+     * @internal
+     */
+    public function getEnded()
+    {
+        return $this->ended;
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function getPriority(): int
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(int $priority): void
+    {
+        $this->priority = $priority;
+    }
+
+}
---
+++ tests/EntityTest.php
@@ -0,0 +1,91 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Ambientia QueueCommand package.
+ *
+ * (c) Ambientia Estonia OÜ
+ */
+
+namespace Ambientia\QueueCommand\Tests;
+
+use Ambientia\QueueCommand\QueueCommandEntity;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+use ReflectionProperty;
+use TypeError;
+
+/**
+ * @author mati.andreas@ambientia.ee
+ */
+class EntityTest extends TestCase
+{
+    public function testId(): void
+    {
+        $entity = new QueueCommandEntity();
+        $this->expectException(TypeError::class);
+        $entity->getId();
+    }
+
+    public function tesService(): void
+    {
+        $entity = new QueueCommandEntity();
+        $this->expectException(TypeError::class);
+        $entity->getService();
+    }
+
+    public function testValues(): void
+    {
+
+        $entity = new QueueCommandEntity();
+
+        $this->assertSame([], $entity->getArguments());
+        $this->assertNull($entity->getTtl());
+        $this->assertNull($entity->getStatus());
+        $this->assertNull($entity->getStarted());
+        $this->assertNull($entity->getEnded());
+        $this->assertNull($entity->getMessage());
+        $this->assertEquals(0, $entity->getPriority());
+
+        $id = rand();
+
+        $rc = new ReflectionProperty(QueueCommandEntity::class, 'id');
+        $rc->setAccessible(true);
+        $rc->setValue($entity, $id);
+        $this->assertEquals($id, $entity->getId());
+
+        $service = uniqid();
+        $entity->setService($service);
+        $this->assertEquals($service, $entity->getService());
+
+        $arguments = [uniqid()];
+        $entity->setArguments($arguments);
+        $this->assertEquals($arguments, $entity->getArguments());
+
+        $ttl = new DateTime();
+        $entity->setTtl($ttl);
+        $this->assertEquals($ttl, $entity->getTtl());
+
+        $status = uniqid();
+        $entity->setStatus($status);
+        $this->assertEquals($status, $entity->getStatus());
+
+        $started = new DateTime();
+        $entity->setStarted($started);
+        $this->assertEquals($started, $entity->getStarted());
+
+        $ended = new DateTime();
+        $entity->setEnded($ended);
+        $this->assertEquals($ended, $entity->getEnded());
+
+        $message = uniqid();
+        $entity->setMessage($message);
+        $this->assertEquals($message, $entity->getMessage());
+
+        $priority = rand();
+        $entity->setPriority($priority);
+        $this->assertEquals($priority, $entity->getPriority());
+
+    }
+
+
+}
